2eb00f545d1489e562a5fab8c46d6b7114988c9c
scp15472 <scp15472@gmail.com>
2018-07-11 22:33:36 +0530

    Student Result

---
+++ StudentResult.java
@@ -0,0 +1,13 @@
+class StudentResult
+{
+	public static void main(String[] args)
+	{
+		String studentName= "Mahesh";
+		byte marks=83;
+		String[] subject={"Java", "Data Structure", "C"};
+		double percentage=83.7;
+		char grade='A';
+		boolean isPassed=true;
+		System.out.println("Name="+studentName + "Marks="+marks + "Percentage="+percentage + "Grade="+grade + "Pass="+isPassed);
+	}
+} 
