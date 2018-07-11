e389dce53bd7e1f92eab42e91f9b21ba5e934d87
scp15472 <scp15472@gmail.com>
2018-07-11 22:29:17 +0530

    Assignment 1

---
+++ BankAccount.java
@@ -0,0 +1,12 @@
+class BankAccount
+{
+	public static void main(String[] args)
+	{
+		String bankName="Bank Of Maharashtra";
+		String accountType="Saving";
+		int accountNumber=1234567890;
+		boolean isCheckBook=true;
+		short balance=10000;
+	System.out.println("Bank Name: "+bankName +"Account Type: "+accountType +"Account Number: "+accountNumber +"Balance: "+balance +"Check Book Request: "+isCheckBook);
+	}
+}
