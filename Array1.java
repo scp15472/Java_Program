ed5e3d12860c6515181a63e870ce2600e2fe6c6f
scp15472 <scp15472@gmail.com>
2018-07-11 22:34:28 +0530

    For Loop 1

---
+++ Array1.java
@@ -0,0 +1,10 @@
+class Array1{
+	public static void main(String[] args){
+		byte i;
+		String[] studName= new String[2];
+		studName[0] = "Vicky";
+		studName[1] = "Shubham";
+		for(i=0;i<5;i++)
+		System.out.println(studName[0] + "  " + studName[1]);
+	}
+}
