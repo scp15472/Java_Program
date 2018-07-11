86ed10c8337528b808c7dc22c6bccb2637598c66
scp15472 <scp15472@gmail.com>
2018-07-11 22:30:33 +0530

    Array Demo

---
+++ DemoArray.java
@@ -0,0 +1,8 @@
+class DemoArray{
+	public static void main(String[] args){
+		String[] studName= new String[2];
+		studName[0] = "Vicky";
+		studName[1] = "Shubham";
+		System.out.println(studName[0] + "  " + studName[1]);
+	}
+}
